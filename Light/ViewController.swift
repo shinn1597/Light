7024db7347a8ed73c448d2694d6b6f094a42a579
Thandar <thandartun197@gmail.com>
2021-02-10 11:59:52 -0500

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Thandar Tun on 2/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
